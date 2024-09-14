0e28243bbc0534a2fa972c301f56d716be40a7f7
Markus Corvinus <markus.corvinus0827@gmail.com>
2024-09-14 09:55:15 +0800

    php tester

---
+++ payment_action.php
@@ -0,0 +1,13 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $amount = htmlspecialchars($_POST['amount']);
+    $payment_type = htmlspecialchars($_POST['payment_type']);
+    $account_number = htmlspecialchars($_POST['account_number']);
+
+    echo "<h2>Your Input:</h2>";
+    echo "amount: " . $amount . "<br>";
+    echo "payment type: " . $payment_type . "<br>";
+    echo "account number: " . $account_number . "<br>";
+}
+?>
